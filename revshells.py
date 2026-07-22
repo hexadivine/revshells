2be694a3feaf6fc9986a0927cec309515e090c6b
hexadivine <hexadivine@gmail.com>
2026-07-22 09:01:54 +0000

    Refactor RevShells to include Listener initialization and update logic

--- revshells.py
+++ revshells.py
@@ -4,6 +4,7 @@ from textual.widgets import Static
 
 from textual import on
 from components.ListenerIPAndPort import ListenerIPAndPort
+from components.Listener import Listener
 from components.PayloadArea import PayloadArea
 
 class RevShells(App):
@@ -13,8 +14,11 @@ class RevShells(App):
         listener_ip_and_port = ListenerIPAndPort(classes='grid grid-1')
         yield listener_ip_and_port
         
+        self.listener = Listener(classes='grid grid-2')
+        yield self.listener
+
         for i in range(0, 5):
-            if i == 0 or i==4:
+            if i == 0 or i==4 or i==1:
                 continue
             yield Static(f"{i+1}", classes=f"grid grid-{i+1}")
 
@@ -23,9 +27,11 @@ class RevShells(App):
     
     @on(ListenerIPAndPort.Update)
     def listener_ip_and_port_update(self, message):
+        self.listener.ip = message.ip
+        self.listener.port = message.port
+        
         self.payload_area.ip = message.ip
         self.payload_area.port = message.port
-        
 
 if __name__ == "__main__":
     app = RevShells()
