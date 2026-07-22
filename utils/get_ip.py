6fa20983e8b32d0890d47e372a74d1e6beae7ade
hexadivine <hexadivine@gmail.com>
2026-07-22 09:30:22 +0000

    Remove debug print statement and handle invalid interface case in get_ip function

--- utils/get_ip.py
+++ utils/get_ip.py
@@ -3,9 +3,8 @@ import subprocess
 
 def get_ip(interface):
     valid_interfaces = get_interfaces()
-    print(valid_interfaces)
     if (interface not in valid_interfaces):
-        raise "InvalidInterface"
+        return ''
     
     cmd = f"ip -4 addr show {interface} | grep 'inet' | cut -d' ' -f 6 | cut -d'/' -f 1"
     ip = subprocess.run(cmd, capture_output=True, shell=True, text=True).stdout.rstrip().lstrip()
